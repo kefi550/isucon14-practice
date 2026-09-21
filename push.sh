86030a3286f8877c8fcd4bc5f4d40e139dbffaf5
kefi550 <ytiryrstd550@gmail.com>
2026-09-21 11:28:04 +0900

    push.sh

---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+
+LOCAL_PATH="."
+LOCK_FILE='~/bench.lock'
+
+if ! ssh isu1 "set -C; : > $LOCK_FILE" 2>/dev/null; then
+  echo 'ベンチマークまたは push が実行中です' >&2
+  exit 1
+fi
+trap 'ssh isu1 "rm -f '$LOCK_FILE'"' EXIT
+
+for i in $(seq 1); do
+  host="isu${i}"
+  echo $host
+  rsync -avr "${LOCAL_PATH}/webapp/go/" ${host}:~/webapp/go/
+  rsync -avr "${LOCAL_PATH}/env.sh" ${host}:~/env.sh
+  ssh ${host} 'bash -l -c "sudo logrotate -f /etc/logrotate.conf; export PATH=/home/isucon/local/go/bin:/home/isucon/go/bin:\$PATH; cd ~/webapp/go; rm isuride; go build; sudo systemctl restart isuride-go.service;"'
+done
+
